7ab6ff789982ef1c1da783e168edb78423cc26a6
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-01 15:37:53 +0200

    attempt refactor fetch in recipe page

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -3,33 +3,47 @@
 import { RecipeType } from "@/utils/types";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { recipeFetcher } from "@/utils/functions";
 
 const recipePage = ({params}: {params:{id:string}}) => {
   const {id} = params;
+  console.log(id);
   const [recipe, setRecipe] = useState<RecipeType | null>(null)
 
-useEffect(() => {  
   const fetchRecipe = async () => {
-    try {
-      if (id) {
-        const response = await fetch(
-          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-        ); // turn this into a function that can take the end of the link
-        const data = await response.json();
-        console.log(data.meals);
-        setRecipe(data.meals[0]);
+    const data = await recipeFetcher({middle: "lookup", end: id});
 
-      }
-    } catch (error) {
-      console.log(error);
+    if (data) {
+      console.log(data)
+      // setRecipe();
+    } else {
+      console.log("No meal found");
     }
   };
+
+  // const fetchRecipe = async () => {
+  //   try {
+  //     if (id) {
+  //       const response = await fetch(
+  //         `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  //       ); // turn this into a function that can take the end of the link
+  //       const data = await response.json();
+  //       console.log(data.meals);
+  //       setRecipe(data.meals[0]);
+        
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {  
   fetchRecipe();
 }, []);
 
 
   return(
-    <div>
+    <div className="flex flex-col  text-black p-4 mx-auto space-y-3">
       <p>Hello from the recipe page. Id number is {id} </p>
       {recipe && 
       <div>
