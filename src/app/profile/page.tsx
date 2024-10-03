cc69e5f1a5ee09af46b9f73147e38c5b137920a0
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-03 10:41:31 +0200

    fetch recipe by id to get name

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,32 +1,55 @@
-'use client'
+"use client";
 
-import Button from "@/Components/Button"
-import { useSavedRecipesContext, useUserContext } from "@/utils/contexts"
-import { SavedRecipesContextType, UserContextType } from "@/utils/types"
-import Link from "next/link"
+import Button from "@/Components/Button";
+import { useSavedRecipesContext, useUserContext } from "@/utils/contexts";
+import { recipeFetcher } from "@/utils/functions";
+import { SavedRecipesContextType, UserContextType } from "@/utils/types";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const profile = () => {
   const { user } = useUserContext() as UserContextType;
-  const {savedRecipes, removeRecipe} = useSavedRecipesContext() as SavedRecipesContextType;
-  console.log(savedRecipes);
+  const { savedRecipes, removeRecipe } =  useSavedRecipesContext() as SavedRecipesContextType;
+  const [recipeNames, setRecipeNames] = useState<Record<string, string>>({}); // record allows to define an oject with keys and values of type string
 
-  
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const names: Record<string, string> = {};
+
+      for (const savedRecipe of savedRecipes) {
+        const data = await recipeFetcher({ action: `lookup.php?i=${savedRecipe.id}`});
+        const recipeName = data.meals[0]?.strMeal || "Unknown Recipe";
+        names[savedRecipe.id] = recipeName;
+      }
+
+      setRecipeNames(names);
+    };
+
+    if (savedRecipes.length > 0) {
+      fetchRecipes();
+    }
+  }, [savedRecipes]);
 
   return (
     <>
       <h1>Profile Page</h1>
       {user ? (
         <div>
-          <p>Your favourite category: <span>{user.category}</span></p>
+          <p>
+            Your favourite category: <span>{user.category}</span>
+          </p>
           <p>Your saved recipes:</p>
           {savedRecipes.length > 0 ? (
             <ul>
               {savedRecipes.map((recipe) => (
                 <li key={recipe.id}>
                   <Link href={`/recipe/${recipe.id}`}>
-                    {recipe.id}
+                    {recipeNames[recipe.id]}
                   </Link>
-                  <Button buttonText="Unsave" onClick={() => removeRecipe(recipe.id)} />
+                  <Button
+                    buttonText="Unsave"
+                    onClick={() => removeRecipe(recipe.id)}
+                  />
                 </li>
               ))}
             </ul>
@@ -38,7 +61,7 @@ const profile = () => {
         <p>Please log in to view your profile and saved recipes.</p>
       )}
     </>
-  )
-}
+  );
+};
 
-export default profile
+export default profile;
