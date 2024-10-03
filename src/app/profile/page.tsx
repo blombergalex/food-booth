d54d33484743017c0e1120dc539e4ae61212e740
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-03 10:28:51 +0200

    implement save functionality

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,22 +1,42 @@
 'use client'
 
-import { useUserContext } from "@/utils/contexts"
-import { UserContextType } from "@/utils/types"
+import Button from "@/Components/Button"
+import { useSavedRecipesContext, useUserContext } from "@/utils/contexts"
+import { SavedRecipesContextType, UserContextType } from "@/utils/types"
+import Link from "next/link"
 
 const profile = () => {
   const { user } = useUserContext() as UserContextType;
+  const {savedRecipes, removeRecipe} = useSavedRecipesContext() as SavedRecipesContextType;
+  console.log(savedRecipes);
+
+  
 
   return (
     <>
       <h1>Profile Page</h1>
       {user ? (
         <div>
-        <p>{user.name}'s saved recipes</p>
-        {/* // show saved recipes: */}
-        <p>Your favourite category:<span>{user.category}</span></p>
-      </div>
-      ) : <p>No user found</p>
-    }
+          <p>Your favourite category: <span>{user.category}</span></p>
+          <p>Your saved recipes:</p>
+          {savedRecipes.length > 0 ? (
+            <ul>
+              {savedRecipes.map((recipe) => (
+                <li key={recipe.id}>
+                  <Link href={`/recipe/${recipe.id}`}>
+                    {recipe.id}
+                  </Link>
+                  <Button buttonText="Unsave" onClick={() => removeRecipe(recipe.id)} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No saved recipes yet.</p>
+          )}
+        </div>
+      ) : (
+        <p>Please log in to view your profile and saved recipes.</p>
+      )}
     </>
   )
 }
--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,38 +1,35 @@
 'use client'
 
-import { RecipeType } from "@/utils/types";
+import { RecipeType, SavedRecipesContextType } from "@/utils/types";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { recipeFetcher } from "@/utils/functions";
+import { useSavedRecipesContext } from "@/utils/contexts";
+import Button from "@/Components/Button";
 
 const recipePage = ({params}: {params:{id:string}}) => {
   const {id} = params;
-  console.log(id);
   const [recipe, setRecipe] = useState<RecipeType | null>(null)
   const [ingredients, setIngredients] = useState<string[]>([])
   const [measures, setMeasures] = useState<string[]>([])
-
+  const {savedRecipes, addRecipe, removeRecipe} = useSavedRecipesContext() as SavedRecipesContextType;
 
   const fetchRecipe = async () => {
     const data = await recipeFetcher({action: `lookup.php?i=${id}`});
-    
-    let meal = data.meals[0];
-    console.log(meal)
-    const ingredientKeys = Object.keys(meal).filter (item => item.includes("strIngredient"))
-    const ingredientValues = [];
-    const measureValues = [];
-    
-    for(let i = 1; i < ingredientKeys.length; i++) {
-      if (meal[ingredientKeys[i]] !== "")
-        ingredientValues.push(meal[ingredientKeys[i]]);
-      measureValues.push(meal['strMeasure' + (i + 1)] || '');
-    } 
+    if (data && data.meals.length > 0) {
+      let meal = data.meals[0];
+      
+      const ingredientKeys = Object.keys(meal).filter (item => item.includes("strIngredient"))
+      const ingredientValues = [];
+      const measureValues = [];
+      
+      for(let i = 1; i < ingredientKeys.length; i++) {
+        if (meal[ingredientKeys[i]] !== "")
+          ingredientValues.push(meal[ingredientKeys[i]]);
+        measureValues.push(meal['strMeasure' + (i + 1)] || '');
+      } 
 
-    console.log(ingredientValues)
-    console.log(measureValues)
-    
-    if (data) {
-      setRecipe(data.meals[0]); 
+      setRecipe(meal); 
       setIngredients(ingredientValues);
       setMeasures(measureValues);
     } else {
@@ -41,8 +38,21 @@ const recipePage = ({params}: {params:{id:string}}) => {
   };
 
   useEffect(() => {  
-  fetchRecipe();
-}, []);
+    fetchRecipe();
+  }, [id]);
+
+  const isRecipeSaved = savedRecipes.some((savedRecipe) => savedRecipe.id === id);
+
+  const handleSaveClick = () => {
+    console.log('clicked save recipe button')
+    if (isRecipeSaved) {
+      removeRecipe(id);
+    } else {
+      addRecipe(id);
+    }
+    console.log(savedRecipes);
+    console.log(id);
+  };
 
   return(
     <div className="flex flex-col text-black p-4 mx-auto space-y-3">
@@ -70,9 +80,8 @@ const recipePage = ({params}: {params:{id:string}}) => {
               ))}
             </ul>
         </div>
+        <Button buttonText={isRecipeSaved ? "Unsave recipe" : "Save recipe"} onClick={handleSaveClick} />
       </div>
-      // Add recipe to favourites button 
-      // handleClick, add recipeId to savedrecipes
       }
     </div>
   )
