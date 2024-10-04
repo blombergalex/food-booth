90bdb465d20ae263eff2f4d232047357ee9e8586
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-04 14:38:36 +0200

    generate recipe by category

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -1,14 +1,18 @@
 'use client'
 
 import { recipeFetcher } from "@/utils/functions";
-import { useEffect } from "react";
+import { RecipeType } from "@/utils/types";
+import Link from "next/link";
+import Image from "next/image"
+import { useEffect, useState } from "react";
 
 const recipiesByCategory = ({params}:{params:{category:string}}) => {
   const {category} = params;
+  const [recipes, setRecipes] = useState<RecipeType[] | null>(null);
   
   const fetchRecipes = async () => {
-    const recipies = await recipeFetcher({action:`filter.php?c=${category}`});
-    console.log(recipies)
+    const recipes = await recipeFetcher({action:`filter.php?c=${category}`});
+    setRecipes(recipes.meals);
   } 
 
   useEffect(() => {
@@ -16,9 +20,32 @@ const recipiesByCategory = ({params}:{params:{category:string}}) => {
   }, [])
   
   return(
-    <div className="p-6">
-      <h3 className="capitalize font-semibold">Our most tasty {category} recipies</h3>
-    </div>
+    <>
+      <h3 className="p-6 capitalize font-semibold">Our most tasty {category} recipies</h3>
+      <div className="p-6 flex flex-wrap">
+        {recipes &&
+          recipes.map((meal: RecipeType) => (
+            <div
+              key={meal.idMeal}
+              className="m-2 p-6 bg-zinc-900 rounded-3xl w-[300px] items-center"
+              >
+              <Link
+                className="flex flex-col font-semibold text-center items-center text-slate-200"
+                href={`/recipe/${meal.idMeal}`}
+                >
+                <Image
+                  src={meal.strMealThumb}
+                  width={220}
+                  height={220}
+                  alt={`Image of ${meal.strMeal}`}
+                  className="rounded-lg m-3"
+                  />
+                {meal.strMeal}
+              </Link>
+            </div>
+          ))}
+      </div>
+      </>
   )
 }
 
