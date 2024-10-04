73cd1e98f79377d7b678e2131cf19134e2bc4996
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-04 08:50:21 +0200

    clean up users data

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -4,16 +4,13 @@ export const registeredUsers: UserType[] = [
   {
     name: "Jimi",
     category: "Seafood",
-    savedRecipes: []
   },
   {
     name: "John",
     category: "Beef",
-    savedRecipes: []
   },
   {
     name: "Ringo",
     category: "Vegan",
-    savedRecipes: []
   }
 ]
