be138b1c40e76ed9d48ee254fc8030aa0c80a88c
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-07 14:21:14 +0200

    add border on button

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -3,7 +3,7 @@ import { ButtonType } from "@/utils/types";
 const Button = ({ buttonText, onClick }: ButtonType) => {
   return (
     <button
-      className="bg-white p-1 border border-1 m-1 rounded-md font-semibold text-zinc-900 min-w-[80px] h-fit outline-none hover:bg-zinc-900 hover:text-white"
+      className="bg-white p-1 border border-1 m-1 border-zinc-900 rounded-md font-semibold text-zinc-900 min-w-[80px] h-fit outline-none hover:bg-zinc-900 hover:text-white"
       onClick={onClick}
     >
       {buttonText}
