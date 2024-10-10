34942c33e8b88068596798c28ec02449ba952b4a
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-10 16:05:56 +0200

    use usepathname to style active menu item

--- src/app/components/Menu/index.tsx
+++ src/app/components/Menu/index.tsx
@@ -1,18 +1,32 @@
 import Link from "next/link";
 import React from "react";
-import Button from "@/app/components/Button";
+import { usePathname } from "next/navigation";
 
 const Menu = () => {
+  const pathname = usePathname();
+
+  const baseClasses = "text-white flex items-center text-lg";
+  const activeClasses = "underline underline-offset-[5px]";
+
   return (
     <nav className="flex justify-evenly py-4 bg-orange-400 sticky top-[136px] md:top-[144px]">
-      <Link href="/">
-        <Button buttonText="Home" onClick={() => {}} />
+      <Link
+        href="/"
+        className={`${baseClasses} ${pathname === "/" ? activeClasses : ""}`}
+      >
+        Home
       </Link>
-      <Link href="/profile">
-        <Button buttonText="Profile" onClick={() => {}} />
+      <Link
+        href="/profile"
+        className={`${baseClasses} ${pathname === "/profile" ? activeClasses : ""}`}
+      >
+        Profile
       </Link>
-      <Link href="/category">
-        <Button buttonText="Category" onClick={() => {}} />
+      <Link
+        href="/category"
+        className={`${baseClasses} ${pathname === "/category" ? activeClasses : ""}`}
+      >
+        Category
       </Link>
     </nav>
   );
